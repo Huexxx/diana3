be87f6ee1307803809256b91d3e0e086a360df01
Huexxx <huexxx@gmail.com>
2013-01-13 15:37:23 +0100

    [08/16] readahead: thrashing safe context readahead

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -924,6 +924,7 @@ struct file_ra_state {
 
 /* ra_flags bits */
 #define	READAHEAD_MMAP_MISS	0x00000fff /* cache misses for mmap access */
+#define READAHEAD_THRASHED	0x10000000
 
 /*
  * Don't do ra_flags++ directly to avoid possible overflow:
