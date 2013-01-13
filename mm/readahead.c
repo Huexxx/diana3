ae35ab4b79fb09774451435e92de39ac06194c39
Huexxx <huexxx@gmail.com>
2013-01-13 15:47:24 +0100

    [10/16] readahead: add tracing event

--- mm/readahead.c
+++ mm/readahead.c
@@ -42,6 +42,9 @@
 #include <linux/pagevec.h>
 #include <linux/pagemap.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/readahead.h>
+
 /*
  * Set async size to 1/# of the thrashing threshold.
  */
@@ -327,6 +330,11 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 		offset += this_chunk;
 		nr_to_read -= this_chunk;
 	}
+
+	trace_readahead(mapping, offset, nr_to_read,
+			RA_PATTERN_FADVISE << READAHEAD_PATTERN_SHIFT,
+			offset, nr_to_read, 0, ret);
+
 	return ret;
 }
 
@@ -353,6 +361,9 @@ unsigned long ra_submit(struct file_ra_state *ra,
 
 	actual = __do_page_cache_readahead(mapping, filp,
 					ra->start, ra->size, ra->async_size);
+
+	trace_readahead(mapping, offset, req_size, ra->ra_flags,
+			ra->start, ra->size, ra->async_size, actual);
 
 	return actual;
 }
